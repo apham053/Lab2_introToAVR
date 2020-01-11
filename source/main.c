f71b6ed9b407178f8f5395855fec9194e299a214
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-01-10 21:35:50 -0800

    now done with exercise 2

--- source/main.c
+++ source/main.c
@@ -10,38 +10,41 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-#endif		
+#endif
 
 int main(void) {
-	DDRA = 0x00;
-	DDRC = 0xFF;
+ 	DDRA = 0x00; 
+ 	DDRC = 0xFF; 
 	PORTA = 0xFF;
 	PORTC = 0x00;
 	
 	unsigned char cntAvail = 4; // 4 parking spaces total
 	
-	unsigned char A3 = PINA & 0x08; //reads PA3
-	unsigned char A2 = PINA & 0x04; 
-	unsigned char A1 = PINA & 0x02;
-	unsigned char A0 = PINA & 0x01; //reads PA0
-	
-	while(1) {
-		if (A3 == 0x08) {
-			cntAvail = cntAvail - 1;
-		}
-		if (A2 == 0x04) {
-			cntAvail = cntAvail - 1;
-		}
-		if (A1 == 0x02) {
-			cntAvail = cntAvail - 1;
-		}
-		if (A0 == 0x01) {
-			cntAvail = cntAvail - 1;
-		}
-	}
-	
-	PORTC = cntAvail;
-	
+ 	while(1) {
+		unsigned char A3 = PINA & 0x08; //reads PA3
+		unsigned char A2 = PINA & 0x04;
+		unsigned char A1 = PINA & 0x02;
+		unsigned char A0 = PINA & 0x01; //reads PA0
+		
+ 		if(A3 == 0x08) {
+ 			cntAvail = cntAvail -1;
+ 		}
+
+ 		if(A2 == 0x04) {
+ 			cntAvail = cntAvail -1;
+ 		}
+
+ 		if(A1 == 0x02) {
+ 			cntAvail = cntAvail -1;
+ 		}
+
+ 		if(A0 == 0x01) {
+ 			cntAvail = cntAvail -1;
+ 		}
+
+ 		PORTC = cntAvail;
+ 		cntAvail = 4; // reset to 4 parking spaces open
+ 	}
 	return 0;
 }
 
