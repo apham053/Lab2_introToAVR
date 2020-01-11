d77699f2423d947839ec63d4e8e0189a38c36cfd
Anthony Pham <apham053@wch136-19.cs.ucr.edu>
2020-01-10 19:23:13 -0800

    Done with exercise 2

--- source/main.c
+++ source/main.c
@@ -1,7 +1,7 @@
 /*	Author: apham053
  *  Partner(s) Name: Steven Rodriguez
  *	Lab Section:
- *	Assignment: Lab #2  Exercise #1
+ *	Assignment: Lab #2  Exercise #2
  *	Exercise Description: [optional - include for your own benefit]
  *
  *	I acknowledge all content contained herein, excluding template or example
@@ -13,29 +13,35 @@
 #endif		
 
 int main(void) {
-	DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs, initialize to 1s
-	DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
-	unsigned char garageDoor = 0x00; // Temporary variable to hold the value of garage door sensor
-	unsigned char lightSensor = 0x00; // Temporary variable to hold the value of light sensor
-	unsigned char LED = 0x00; // Temporary variable to hold the value of LED
-while(1) {
-		// 1) Read input
-		garageDoor = PINA & 0x01; // reads PA0
-		lightSensor = PINA & 0x02; // reads PA1
-		
-		// 2) Perform computation
-		if ((garageDoor == 0x01) && (lightSensor == 0x00)) {
-			LED = (LED & 0xFE) | 0x01;
-			//LED = 0x01;
+	DDRA = 0x00;
+	DDRC = 0xFF;
+	PORTA = 0xFF;
+	PORTC = 0x00;
+	
+	unsigned char cntAvail = 4; // 4 parking spaces total
+	
+	unsigned char A3 = PINA & 0x08; //reads PA3
+	unsigned char A2 = PINA & 0x04; 
+	unsigned char A1 = PINA & 0x02;
+	unsigned char A0 = PINA & 0x01; //reads PA0
+	
+	while(1) {
+		if (A3 == 0x08) {
+			cntAvail = cntAvail - 1;
 		}
-		else {
-			LED = (LED & 0xFE) | 0x00;
-			//LED = 0x00;
+		if (A2 == 0x04) {
+			cntAvail = cntAvail - 1;
 		}
-		
-	    // 3) Write output
-		PORTB = LED;	
+		if (A1 == 0x02) {
+			cntAvail = cntAvail - 1;
+		}
+		if (A0 == 0x01) {
+			cntAvail = cntAvail - 1;
+		}
 	}
+	
+	PORTC = cntAvail;
+	
 	return 0;
 }
 
